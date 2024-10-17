296363cacd5460f59c1e58dc6b68948a2d2796d5
MehtarRaditya <ryu.mehtar.2006@gmail.com>
2024-10-18 01:11:24 +0700

    Data Type

---
+++ Kapal/Kapal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    float waktu, kecepatan;
+    float jarak, pertamaxTurbo;
+
+    printf("Masukkan waktu lalu kecepatan: ");
+    scanf("%f %f", &waktu, &kecepatan);
+
+    printf("Waktu: %f jam\n", waktu);
+    printf("Kecepatan: %f rata-rata\n", kecepatan);
+
+    jarak = waktu*kecepatan;
+    pertamaxTurbo = jarak/12;
+
+    printf("Jarak: %.3f Km\n", jarak);
+    printf("Pertamax Turbo yang dihabsikan: %.3f L\n", pertamaxTurbo);
+
+    return 0;
+}
